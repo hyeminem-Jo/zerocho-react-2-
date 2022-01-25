043262a625cd04b248f3800ba124c7995b3914a0
hyeminem-Jo <konnimey@naver.com>
2022-01-25 15:51:20 +0900

    Feat: response-check_final

--- ResponseCheck.jsx
+++ ResponseCheck.jsx
@@ -1,85 +1,82 @@
-import React, { Component } from "react";
+import React, { useRef, useState } from "react";
 
-class ResponseCheck extends Component {
-  state = {
-    state: "waiting",
-    message: "클릭해서 시작하세요",
-    result: [],
-  };
+const ResponseCheck = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState("클릭해서 시작하세요");
+  const [result, setResult] = useState([]);
+  const timeOut = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+  // ref 는 보통 DOM 에 접근할 때만 사용되었는데, hooks 에서는 this 의 속성들을 ref 로 표현한다. (ref 의 추가적인 기능)
+  // ref 를 쓸 땐 current 를 붙여주어야 한다.
 
-  timeout;
-  startTime;
-  endTime;
-  // startTime 역시 변하는 부분이긴 하지만, state 로 만들어버리면 렌더링이 다시 되기 때문에 그냥 this.startTime 으로 만들어준다. (re-rendering 을 겪고 싶지 않을 땐 this.~ 로 만들어준다.)
-  // startTime 의 변화로 화면에 변화를 주는 것은 아니기 때문에 굳이 state 로 만들 필요 x
-
-  onClickScreen = () => {
-    const { state, message, result } = this.state;
+  const onClickScreen = () => {
     if (state === "waiting") {
-      this.setState({
-        state: "ready",
-        message: "초록색이 되면 클릭하세요",
-      });
-      this.timeout = setTimeout(() => {
-        this.setState({
-          state: "now",
-          message: "지금 클릭",
-        });
-        this.startTime = new Date(); // 시작 시간
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요");
+      timeOut.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금 클릭");
+        startTime.current = new Date(); // 시작 시간
       }, Math.floor(Math.random() * 1000) + 2000); // 2 ~ 3초
     } else if (state === "ready") {
       // 성급하게 클릭 했을 때
-      // 이때 처음 화면으로 돌아가야 하는데, 이전에 생긴 setTimeout 이 작동되어버려 초기화면이 아닌 초록색 화면이 나와버린다. 이를 방지하기 위해 기존에 있던 setTimeout 을 우선 초기화 시켜준다.
-      clearTimeout(this.timeout);
-      this.setState({
-        state: "waiting",
-        message:
-          "너무 성급하셨군요. 초록색이 된 후에 클릭하세요 (지금 다시 클릭)",
-      });
+      clearTimeout(timeOut.current);
+      setState("waiting");
+      setMessage(
+        "너무 성급하셨군요. 초록색이 된 후에 클릭하세요 (지금 다시 클릭)"
+      );
     } else if (state === "now") {
       // 반응 속도 체크 구간
-      this.endTime = new Date(); // 끝 시간
-      this.setState((prevState) => {
-        return {
-          state: "waiting",
-          message: "클릭해서 시작하세요",
-          result: [...prevState.result, this.endTime - this.startTime],
-        }; // 옛날 배열을 추가하는 것이므로 함수형 setState 로 전환, prev 사용
-      });
+      endTime.current = new Date(); // 끝 시간
+      setState("waiting");
+      setMessage("클릭해서 시작하세요");
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current];
+      }); // 옛날 배열을 추가하는 것이므로 함수형 setState 로 전환, prev 사용
     }
   };
 
-  onReset = () => {
-    this.setState({
-      result: [],
-      // reseult 가 빈 배열이 되면서 초기화 된다.
-    })
-  }
+  const onReset = () => {
+    setResult([]);
+  };
 
-  renderAverage = () => {
-    const { result } = this.state;
+  const renderAverage = () => {
     return result.length === 0 ? null : (
       <>
         <div>
-          평균 시간: {result.reduce((a, c) => a + c) / this.state.result.length}
+          평균 시간: {result.reduce((a, c) => a + c) / result.length}
           ms
         </div>
-        <button onClick={this.onReset}>리셋하기</button>
+        <button onClick={onReset}>리셋하기</button>
       </>
     );
   };
 
-  render() {
-    const { state, message } = this.state;
-    return (
-      <>
-        <div id="screen" className={state} onClick={this.onClickScreen}>
-          {message}
-        </div>
-        {this.renderAverage()}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {/* {(() => {
+        if (result.length === 0) {
+          return null;
+        } else {
+          return (
+            <>
+              <div>
+                평균 시간: {result.reduce((a, c) => a + c) / result.length}
+                ms
+              </div>
+              <button onClick={onReset}>리셋하기</button>
+            </>
+          );
+        }
+      })()} */}
+      {renderAverage()}
+    </>
+  );
+};
 
 export default ResponseCheck;
+
